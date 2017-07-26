7e0b4a317f6ec26048de03c27a3cfb33cd42ca7d
Duc Nguyen <d4nnguyen@gmail.com>
2017-07-26 14:51:40 +0700

    Initialize firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD8RTY6X8Lr65weho0nWWIAHSJ92-65LTs",
+    authDomain: "a-short-nap.firebaseapp.com",
+    databaseURL: "https://a-short-nap.firebaseio.com",
+    projectId: "a-short-nap",
+    storageBucket: "a-short-nap.appspot.com",
+    messagingSenderId: "879877696188"
+  }
 };
